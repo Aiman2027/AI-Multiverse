85c59f5fcea3511b1eaf4d83da6e0e6a31b552a8
Aiman2027 <aimannishat78692@gmail.com>
2026-07-10 16:12:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+st.title("AI Multiverse ")
+
+personality=st.selectbox("Who do you want to talk to?",[
+    "An Expert Hacker", "A HR", "A crazy Tech fresher"
+])
+
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+client=genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+user_message=st.text_input("Say something:")
+if st.button("SEND"):
+    if user_message:
+        ai_instructions= f"You are acting as {personality}.Respond to the message sent by the user staying completely in character: {user_message} "
+        with st.spinner("Connecting to the multiverse!........"):
+            response=client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=ai_instructions
+            )
+
+            st.success("Message received")
+            st.write(response.text)
+
+
+    else:
+        st.warning("please enter message first")
+
+
